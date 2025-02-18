5a73502c2a638cb1dfeedd6a5f9c0dcc65ecbf50
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-18 13:02:38 +1000

    Feat: Created Class Game and Function __init__
    Desc: Game Class, New Function for initializing general pygame setup.

--- Code/Main.py
+++ Code/Main.py
@@ -1,16 +1,18 @@
 import pygame, sys
 
-pygame.init() # Initialize pygame module.
-screen = pygame.display.set_mode((1200,600)) # Creates the display (Width,Height)
-clock = pygame.time.Clock() # Creates internal Clock for fps/tick system
-pygame.display.set_caption("Test Game") # Display Window Title
+class Game:
+    def __init__(self):
+        pygame.init() # Initialize pygame module.
+        screen = pygame.display.set_mode((1200,600)) # Creates the display (Width,Height)
+        clock = pygame.time.Clock() # Creates internal Clock for fps/tick system
+        pygame.display.set_caption("Test Game") # Display Window Title
 
-def run(self):
-    while True: # Game runs inside of this loop.
-        for event in pygame.event.get(): # Loops through all events in pygame module
-            if event.type == pygame.QUIT: # Finds event QUIT (X button on window)
-                pygame.quit() # Close window
-                sys.exit() # Exits Code
+    def run(self):
+        while True: # Game runs inside of this loop.
+            for event in pygame.event.get(): # Loops through all events in pygame module
+                if event.type == pygame.QUIT: # Finds event QUIT (X button on window)
+                    pygame.quit() # Close window
+                    sys.exit() # Exits Code
 
-        pygame.display.update() # Takes evrything and updates display surface.
-        self.clock.tick(60) # Setting internal clock to 60fps ensuring while loop doesnt run more than 60 times per second.
+            pygame.display.update() # Takes evrything and updates display surface.
+            self.clock.tick(60) # Setting internal clock to 60fps ensuring while loop doesnt run more than 60 times per second.
