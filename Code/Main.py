a166af84663ef30c60205d56d4cb1ca475a50688
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-18 11:31:39 +1000

    Feat: Display Loop
    Desc: Added while loop which  updates the display.
    quits Game if Event Quit(X button) is pressed

--- Code/Main.py
+++ Code/Main.py
@@ -2,3 +2,10 @@ import pygame
 
 pygame.init() # Initialize pygame module.
 screen = pygame.display.set_mode((1200,600))
+
+while True: # Game runs inside of this loop.
+    for event in pygame.event.get(): # Loops through all events in pygame module
+        if event.type == pygame.QUIT: # Finds event QUIT (X button on window)
+            pygame.quit() # Close window
+
+    pygame.display.update() # Takes evrything and updates display surface.
