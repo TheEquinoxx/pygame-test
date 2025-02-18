b35f4adabdaf3169760f36774e0685cc1318c73b
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-18 13:08:45 +1000

    Feat: Added file check and game class instance
    Desc: Activated run function from created game class instance if name or file is main

--- Code/Main.py
+++ Code/Main.py
@@ -2,17 +2,21 @@ import pygame, sys
 
 class Game:
     def __init__(self):
-        pygame.init() # Initialize pygame module.
+        pygame.init() # Initialize pygame module
         screen = pygame.display.set_mode((1200,600)) # Creates the display (Width,Height)
         clock = pygame.time.Clock() # Creates internal Clock for fps/tick system
         pygame.display.set_caption("Test Game") # Display Window Title
 
     def run(self):
-        while True: # Game runs inside of this loop.
+        while True: # Game runs inside of this loop
             for event in pygame.event.get(): # Loops through all events in pygame module
                 if event.type == pygame.QUIT: # Finds event QUIT (X button on window)
                     pygame.quit() # Close window
                     sys.exit() # Exits Code
 
-            pygame.display.update() # Takes evrything and updates display surface.
-            self.clock.tick(60) # Setting internal clock to 60fps ensuring while loop doesnt run more than 60 times per second.
+            pygame.display.update() # Takes evrything and updates display surface
+            self.clock.tick(60) # Setting internal clock to 60fps ensuring while loop doesnt run more than 60 times per second
+
+if __name__ == "__Main__": # Check if Main file
+    game = Game() # Creating instance of Game class
+    game.run() # run function from game class instance
