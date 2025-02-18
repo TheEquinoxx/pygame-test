0773aba28cd18d039c83162d23a8e4d61e8106bb
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-18 12:54:55 +1000

    BugFix: Changes exit() in while loop to sys.exit()

--- Code/Main.py
+++ Code/Main.py
@@ -9,7 +9,7 @@ while True: # Game runs inside of this loop.
     for event in pygame.event.get(): # Loops through all events in pygame module
         if event.type == pygame.QUIT: # Finds event QUIT (X button on window)
             pygame.quit() # Close window
-            exit() # Exits Code
+            sys.exit() # Exits Code
 
     pygame.display.update() # Takes evrything and updates display surface.
     clock.tick(60) # Setting internal clock to 60fps ensuring while loop doesnt run more than 60 times per second.
