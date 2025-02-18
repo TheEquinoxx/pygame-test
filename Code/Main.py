6b38aee40de57b05c4f03d13aa7be47e069b9c1e
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-18 11:35:23 +1000

    BugFix: Fixed bug causing init error
    Desc: pygame.quit call uninitializing pygame but display.update still runs in while loop causing error.
    after pygame.quit now calls sys.exit to quit code.

--- Code/Main.py
+++ Code/Main.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit
 
 pygame.init() # Initialize pygame module.
 screen = pygame.display.set_mode((1200,600))
@@ -7,5 +8,6 @@ while True: # Game runs inside of this loop.
     for event in pygame.event.get(): # Loops through all events in pygame module
         if event.type == pygame.QUIT: # Finds event QUIT (X button on window)
             pygame.quit() # Close window
+            exit()
 
     pygame.display.update() # Takes evrything and updates display surface.
