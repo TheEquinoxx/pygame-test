9e5ff5496f2c0674b98bd2262bcec8b7f95be9a2
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-18 12:00:01 +1000

    Feat: Added Title Display Name.

--- Code/Main.py
+++ Code/Main.py
@@ -4,6 +4,7 @@ from sys import exit
 pygame.init() # Initialize pygame module.
 screen = pygame.display.set_mode((1200,600)) # Creates the display (Width,Height)
 clock = pygame.time.Clock() # Creates internal Clock for fps/tick system
+pygame.display.set_caption("Test Game")
 
 while True: # Game runs inside of this loop.
     for event in pygame.event.get(): # Loops through all events in pygame module
@@ -12,4 +13,4 @@ while True: # Game runs inside of this loop.
             exit()
 
     pygame.display.update() # Takes evrything and updates display surface.
-    clock.tick(60)
+    clock.tick(60) # Setting internal clock to 60fps ensuring while loop doesnt run more than 60 times per second.
